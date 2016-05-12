da343c13dea36a0e5fcf3b3049f5c6d691eb16c1
RomanKol <kollatschny.roman@web.de>
2016-05-12 15:40:05 +0200

    Failing the test

---
+++ test/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Load Chai module and set expect as our assertion style
+// require('ModuleName')
+let expect = require('chai').expect;
+
+// Load our application
+// require('pathToApplication')
+let App = require('./../app/app.js');
+
+// Define our first test suite
+// describe('testSuite')
+describe('helloWorld app testsuite', function() {
+
+  // Define our first test case
+  // it('testCase')
+  it('returns the string "Hello World"', function() {
+
+    // Define our first test
+    // except(application).to...
+    expect(App)
+      .to.be.a('string')
+      .and.is.equal('Hello World');
+
+  });
+
+});
